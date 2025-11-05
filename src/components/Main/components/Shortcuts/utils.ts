0e8817041c5b823934c43f0feb85bfbd698fae72
Oleg Shilov <shilov-1@yandex.ru>
2025-11-05 18:55:42 +0300

    fixed labels;

--- src/components/Main/components/Shortcuts/utils.ts
+++ src/components/Main/components/Shortcuts/utils.ts
@@ -1,16 +1,18 @@
-import { type SettingsForSave, getCmdOrCtrl } from 'utils';
+import { type Settings, getCmdOrCtrl } from 'utils';
+import { getLabelByAccel } from 'components/Config/components/HotkeyInput/utils.ts';
 
-function getLabel(hotkey: string, radioHotkey: string) {
-  return `${radioHotkey} and ${hotkey}`;
+function getLabel(hotkey: string, doubleHotkey: string) {
+  return `${doubleHotkey} and ${hotkey}`;
 }
 
-export function getLabelForShortcuts(settings: SettingsForSave) {
-  const { hotkey, radioHotkey } = settings;
+export function getLabelForShortcuts(settings: Settings) {
+  const { hotkey, doubleHotkey } = settings;
 
   const defaultKey = getCmdOrCtrl();
+  const hotKeyByAccel = getLabelByAccel(hotkey || defaultKey);
 
-  const safeHotkey = hotkey ?? `${defaultKey} + Shift + V`;
-  const safeRadioHotkey = radioHotkey ?? `Double ${defaultKey}`;
+  const safeHotkey = hotKeyByAccel ?? `${defaultKey} + Shift + V`;
+  const safeDoubleHotkey = doubleHotkey ? `Double ${doubleHotkey}` : `Double ${defaultKey}`;
 
-  return getLabel(safeHotkey, safeRadioHotkey);
+  return getLabel(safeHotkey, safeDoubleHotkey);
 }
